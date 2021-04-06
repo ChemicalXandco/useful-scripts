53110b167e57e7fad47c53a5f221c6b48d488704
ChemicalXandco <32775248+ChemicalXandco@users.noreply.github.com>
2021-04-06 21:24:16 +0100

    debian python env

--- bash/python.py
+++ bash/python.py
@@ -1,10 +1,15 @@
 import bash
 from utils.config import ConfigEditor
+from utils.env import is_exe
 
 def run():
     with ConfigEditor(bash.bashrc) as cfg_edit:
-        cfg_edit.add("alias python='export PYTHONPATH=$PWD && python'", under='# python env')
         cfg_edit.add("alias pytest='export PYTHONPATH=$PWD && pytest'", under='# python env')
+
+        if is_exe('python3') and not is_exe('python'):
+            cfg_edit.add("alias python='export PYTHONPATH=$PWD && python3'", under='# python env')
+        else:
+            cfg_edit.add("alias python='export PYTHONPATH=$PWD && python'", under='# python env')
 
 if __name__ == '__main__':
     run()
