dc0f2bb02e63cd8cdbe4719ef52341ff3e6627a8
ChemicalXandco <32775248+ChemicalXandco@users.noreply.github.com>
2021-05-08 19:23:22 +0100

    only read firefox config if firefox exists

--- firefox/__init__.py
+++ firefox/__init__.py
@@ -5,18 +5,18 @@ import os
 from firefox import prefs
 from utils.env import is_exe, do_nothing
 
-root = Path('~/.mozilla/firefox').expanduser()
-
-installs = os.path.join(root, 'installs.ini')
-_installs_cfg = ConfigParser()
-_installs_cfg.read(installs)
-
-default = os.path.join(root, _installs_cfg[_installs_cfg.sections()[0]]['Default'])
-
 def _run():
     prefs.run()
 
 if is_exe('firefox'):
+    root = Path('~/.mozilla/firefox').expanduser()
+
+    installs = os.path.join(root, 'installs.ini')
+    _installs_cfg = ConfigParser()
+    _installs_cfg.read(installs)
+
+    default = os.path.join(root, _installs_cfg[_installs_cfg.sections()[0]]['Default'])
+
     run = _run
 else:
     run = do_nothing
