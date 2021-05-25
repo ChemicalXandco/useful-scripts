384b1ed3497c4c730ed99e4a08444226112238a7
ChemicalXandco <32775248+ChemicalXandco@users.noreply.github.com>
2021-05-25 22:12:04 +0100

    [bash] only run if bash exists

--- bash/__init__.py
+++ bash/__init__.py
@@ -1,10 +1,16 @@
 from pathlib import Path
 
 from bash import history, prompt, python
+from utils.env import is_exe, do_nothing
 
 bashrc = str(Path('~/.bashrc').expanduser())
 
-def run():
+def _run():
     history.run()
     prompt.run()
     python.run()
+
+if is_exe('bash'):
+    run = _run
+else:
+    run = do_nothing
