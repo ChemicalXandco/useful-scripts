0475d68875d1fd78a67c6b4c41928eca2e4691ce
ChemicalXandco <32775248+ChemicalXandco@users.noreply.github.com>
2021-06-18 11:51:17 +0100

    [firefox] handle exception when firefox is not set up

--- config/firefox/__init__.py
+++ config/firefox/__init__.py
@@ -4,13 +4,19 @@ import os
 from config.firefox import prefs
 from utils.env import dotfile_path
 
-root = dotfile_path('.mozilla/firefox')
+try:
+    root = dotfile_path('.mozilla/firefox')
 
-installs = os.path.join(root, 'installs.ini')
-_installs_cfg = configparser.ConfigParser()
-_installs_cfg.read(installs)
+    installs = os.path.join(root, 'installs.ini')
+    _installs_cfg = configparser.ConfigParser()
+    _installs_cfg.read(installs)
 
-default = os.path.join(root, _installs_cfg[_installs_cfg.sections()[0]]['Default'])
+    default = os.path.join(root, _installs_cfg[_installs_cfg.sections()[0]]['Default'])
 
-def run():
-    prefs.run()
+    def run():
+        prefs.run()
+except Exception:
+    print('Error: need to setup firefox')
+
+    def run():
+        pass
