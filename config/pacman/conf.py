2c125d9c53f3f1485382cf6200cfd7a6a0d96cf6
ChemicalXandco <32775248+ChemicalXandco@users.noreply.github.com>
2021-06-21 22:10:26 +0100

    [pacman] enable parallel downloads

--- config/pacman/conf.py
+++ config/pacman/conf.py
@@ -2,8 +2,11 @@ from utils.config import ConfigEditor
 
 def run():
     with ConfigEditor('/etc/pacman.conf', '# ') as cfg_edit:
+        under = '[options]'
         # Progress bar easter egg
-        cfg_edit.add('ILoveCandy', under='[options]')
+        cfg_edit.add('ILoveCandy', under=under)
+        # Faster download
+        cfg_edit.add('ParallelDownloads = 5', under=under)
 
 if __name__ == '__main__':
     run()
