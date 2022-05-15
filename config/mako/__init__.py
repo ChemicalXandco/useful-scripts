f6f5a13ee88e4e7db657d57a66baf629ed4b7668
ChemicalXandco <32775248+ChemicalXandco@users.noreply.github.com>
2022-05-15 21:10:52 +0100

    [mako] anchor bottom right

--- config/mako/__init__.py
+++ config/mako/__init__.py
@@ -5,5 +5,6 @@ config_path = dotfile_path('.mako/config')
 
 def run():
     with ConfigEditor(config_path) as cfg_edit:
-        cfg_edit.add('layer=overlay')
+        cfg_edit.add('anchor=bottom-right')
         cfg_edit.add('default-timeout=10000')
+        cfg_edit.add('layer=overlay')
